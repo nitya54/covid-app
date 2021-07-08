c1cc70a63663360351860fbe0daad3849ede34e1
nitya <nityadtt@gmail.com>
2021-07-08 16:30:41 +0530

    covid app

---
+++ src/StateWiseData/statewise.js
@@ -0,0 +1,60 @@
+import React, {useEffect,useState} from 'react'
+import "./statewise.css"
+const StateWise = ()=> {
+    const [data,setData]=  useState([])
+    const getCovidData=async() =>{
+        const res= await fetch('https://api.covid19india.org/data.json');
+        const actualData = await res.json();
+        console.log(actualData.statewise)
+        setData(actualData.statewise)
+    }
+    useEffect(() =>{
+            getCovidData()
+    },[]);
+
+    return (
+        <>
+            
+             <div className="container-fluid mt-5">
+                 <div className ="container">
+                     <h1 className="mb-5 text-center text-white">INDIA COVID-19 Dashboard</h1>
+                     </div>
+                <div className="table-responsive bg-white">
+                    <table className="table table-striped table-hover">
+                        <thead className="table-dark">
+                            <tr>
+                                <th>State</th>
+                                <th>Confirmed</th>
+                                <th>Recovered</th>
+                                <th>Deaths</th>
+                                <th>Active</th>
+                                <th>Updated</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                data.map((curElem,ind)=>{
+                                    return(
+                                    <tr key={ind}>
+                                        <td>{curElem.state}</td>
+                                        <td>{curElem.confirmed}</td>
+                                        <td>{curElem.recovered}</td>
+                                        <td>{curElem.deaths}</td>
+                                        <td>{curElem.active}</td>
+                                        <td>{curElem.lastupdatedtime}</td>
+                                    </tr>
+                                    )
+                                })
+                            }
+                       
+                        </tbody>
+                    </table>
+                </div>
+                 </div>
+         
+        </>
+        
+    )
+}
+
+export default StateWise
